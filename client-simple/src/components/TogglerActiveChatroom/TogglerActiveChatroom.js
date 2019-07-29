6340beca0a2f898ff463f8092d44134c33ca810e
martynaroj <martynaxroj@gmail.com>
2019-07-29 14:25:18 +0200

    Create toggler to active/deactive chatroom

---
+++ client-simple/src/components/TogglerActiveChatroom/TogglerActiveChatroom.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ActiveSvg from "../../assets/svg/active.svg";
+import NonActiveSvg from "../../assets/svg/not-active.svg";
+import "./TogglerActiveChatroom.style.scss";
+
+const Toggler = ({ isChecked, toggleActiveChatroom }) => {
+  return (
+    <label className="toggler__wrapper">
+      <img src={ActiveSvg} className="toggler__icon" alt="" />
+
+      <div className="toggler">
+        <input type="checkbox" checked={isChecked ? "checked" : ""} onChange={toggleActiveChatroom} className="toggler__checkbox" />
+        <span className="toggler__lever"></span>
+      </div>
+
+      <img src={NonActiveSvg} className="toggler__icon" alt="" />
+    </label>
+  );
+};
+
+export default Toggler;
