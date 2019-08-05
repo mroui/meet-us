2be4ac399ea7042d2e5c8467a171e710f94431b9
martynaroj <martynaxroj@gmail.com>
2019-08-05 11:54:09 +0200

    Add edit event form validation

--- client-simple/src/pages/Chat/index.js
+++ client-simple/src/pages/Chat/index.js
@@ -180,6 +180,54 @@ class Chat extends Component {
   }
 
 
+  handleFormEditEvent = () => {
+    if (this.valueValid()) console.log("editing")
+  }
+
+
+  valueValid() {
+    const { tempTitle, tempDate, tempTime, tempPrice, tempContact } = this.state;
+
+    if (tempTitle.length <= 3) {
+      message.error(`Event title is too short`);
+      return false;
+    }
+    if (tempDate==null) {
+      message.error(`Event date format is incorrect`);
+      return false;
+    }
+    if (tempTime==null) {
+      message.error(`Event time format is incorrect`);
+      return false;
+    }
+    if (tempPrice >= 10000) {
+      message.error(`Event price is too big`);
+      return false;
+    }
+    if (tempPrice < 0) {
+      message.error(`Event price is too small`);
+      return false;
+    }
+    if (tempContact.length <= 3) {
+      message.error(`Contact details are too short`);
+      return false;
+    }
+
+    return true;
+  }
+
+
+  getTodayDate = () => {
+    const today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth()+1;
+    const yyyy = today.getFullYear();
+    if(dd<10) dd="0"+dd;
+    if(mm<10) mm="0"+mm;
+    return (yyyy+"-"+mm+"-"+dd);
+  }
+
+
   renderModal() {
     const modalOpen = this.state.modalOpen;
     const chatroom = this.state.chatroom ? this.state.chatroom : this.props.chatroom;
@@ -195,48 +243,58 @@ class Chat extends Component {
         desc={modalDesc}
         modalOpen={modalOpen}
         closeModal={this.toggleModal}>
-        <Form onSubmit={this.handleFormSubmit}>
-          <FormInput
-            label="Title"
-            id="title"
-            value={tempTitle}
-            onChange={e => this.setState({tempTitle: e.target.value})}
-          />
-          <FormInput
-            label="Description"
-            id="description"
-            value={tempDesc}
-            onChange={e => this.setState({tempDesc: e.target.value})}
-          />
-          <FormInput
-            label="Date"
-            id="date"
-            type="date"
-            value={tempDate}
-            onChange={e => this.setState({tempDate: e.target.value})}
-          />
-          <FormInput
-            label="Time"
-            id="time"
-            type="time"
-            value={tempTime}
-            onChange={e => this.setState({tempTime: e.target.value})}
-          />
-          <FormInput
-            label="Price"
-            id="price"
-            type="number"
-            value={tempPrice}
-            onChange={e => this.setState({tempPrice: e.target.value})}
-          />
-          <FormInput
-            label="Contact"
-            id="contact"
-            value={tempContact}
-            onChange={e => this.setState({tempContact: e.target.value})}
-          />
-          <Button variant="primary" type="submit" additionalClass="modal__btn">Accept changes</Button>
-        </Form>
+        <FormInput
+          label="Title"
+          id="title"
+          placeholder="Name your event"
+          minLength="3"
+          value={tempTitle}
+          onChange={e => this.setState({tempTitle: e.target.value})}
+        />
+        <FormInput
+          label="Description"
+          id="description"
+          placeholder="Cool things about your event..."
+          value={tempDesc}
+          onChange={e => this.setState({tempDesc: e.target.value})}
+        />
+        <FormInput
+          label="Date"
+          id="date"
+          type="date"
+          min={this.getTodayDate()}
+          value={tempDate}
+          onChange={e => this.setState({tempDate: e.target.value})}
+        />
+        <FormInput
+          label="Time"
+          id="time"
+          type="time"
+          value={tempTime}
+          onChange={e => this.setState({tempTime: e.target.value})}
+        />
+        <FormInput
+          label="Price [$]"
+          id="price"
+          type="number"
+          min="0"
+          max="10000"
+          value={tempPrice}
+          onChange={e => this.setState({tempPrice: e.target.value})}
+        />
+        <FormInput
+          label="Contact"
+          id="contact"
+          placeholder="Telephone number, e-mail address..."
+          minLength="3"
+          value={tempContact}
+          onChange={e => this.setState({tempContact: e.target.value})}
+        />
+        <Button 
+          variant="primary" 
+          type="submit" 
+          additionalClass="modal__btn" 
+          onClick={this.handleFormEditEvent}>Accept changes</Button>
       </Modal>
     );
   }
