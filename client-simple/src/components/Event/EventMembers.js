2ca01cfc80c1af5655e5b6b80fc50f19b4dd1f38
martynaroj <martynaxroj@gmail.com>
2019-08-07 15:06:14 +0200

    Create event members component

---
+++ client-simple/src/components/Event/EventMembers.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class EventMembers extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default EventMembers
