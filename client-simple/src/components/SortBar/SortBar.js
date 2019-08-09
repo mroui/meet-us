9b19b88e3ab088278aa4479b07a60bff1edf456c
martynaroj <martynaxroj@gmail.com>
2019-08-09 14:20:52 +0200

    Modify sorting by added select component

--- client-simple/src/components/SortBar/SortBar.js
+++ client-simple/src/components/SortBar/SortBar.js
@@ -1,19 +1,32 @@
 import React, { Component } from "react";
 import "./SortBar.style.scss";
-import Button from "../../components/Button/Button";
-import arrowDown from "../../assets/svg/arrow_down.svg";
-import arrowUp from "../../assets/svg/arrow_up.svg";
 
 
 class SortBar extends Component {
+  
+  state = {
+    sortOptions: [
+      "Date: the latest",
+      "Date: the earliest",
+      "Location: the nearest",
+      "Location: the farthest",
+      "Price: the cheapest",
+      "Price: the expensive ones",
+      "Added date: the newest",
+      "Added date: the oldest"
+    ]
+  }
+
+
   render() {
     return (
       <div className="bar">
         <h3>Sort by:</h3>
-        <Button additionalClass="bar__button">
-          Distance
-          <img className="bar__icon" src={arrowDown} />
-        </Button>
+        <select className="bar__select">
+          {this.state.sortOptions.map( (title, key) => {
+            return <option key={key} value={title}>{title}</option>
+          })}
+        </select>
       </div>
     )
   }
